ca60868277d104b297715aa56a075eabd558b5ec
juancruzroldan95 <juancruz.roldan19@gmail.com>
2022-10-20 22:21:18 -0300

    quickSort Done

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,41 +1,25 @@
 'use strict'
 // No cambies los nombres de las funciones.
 
-function partition (array) {
-  let pivotIndex = array.length - 1;
-  let i = 0;
-  let j = 1;
-
-  while (j < pivotIndex) {
-    if (array[j] > array[pivotIndex]) {
-      j++;
-    }
-    else {
-      [array[j], array[i]] = [array[i], array[j]];
-      i++;
-      j++;
-    }
-  }
-  [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
-  return i;
-}
-
 function quickSort(array) {
   // Implementar el método conocido como quickSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  if (1 < array.length) {
-    let pivotIndex = partition(array);
-    let leftArray = array.slice(0, pivotIndex);
-    let rightArray = array.slice(pivotIndex + 1, array.length - 1);
-    quickSort(leftArray);
-    quickSort(rightArray);
+  if (array.length <= 1) return array;
+  let pivot = array[0];
+  let subLeft = [];
+  let subRight = [];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] <= pivot) {
+      subLeft.push(array[i]);
+    }
+    else {
+      subRight.push(array[i]);
+    }
   }
-  return array;
+  return quickSort(subLeft).concat(pivot).concat(quickSort(subRight));
 }
-
-quickSort([5, 1, 4, 2, 8]);
 
 function merge (a,b) {
   var result = [];
@@ -50,9 +34,9 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
   if (array.length < 2) return array;
-  var mid = Math.floor(array.length /2);
-  var subLeft = mergeSort(array.slice(0,mid));
-  var subRight = mergeSort(array.slice(mid));
+  let mid = Math.floor(array.length /2);
+  let subLeft = mergeSort(array.slice(0,mid));
+  let subRight = mergeSort(array.slice(mid));
   return merge(subLeft, subRight);
 }
 
